a1078971280e538e1a1fae1ed35722f104fe1c91
Aleksandr <lepplazwer@yandex.ru>
2025-01-23 19:12:21 +0300

    - Переменные и методы стали статическими, добавлен новый метод tranferMoney для перевода и loadAccount для загрузки аккаунта

--- src/Bank/BankAccount.java
+++ src/Bank/BankAccount.java
@@ -1,19 +1,26 @@
 package Bank;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
-    private final int accNumber;
-    private volatile double balance;
-    private final Lock locker = new ReentrantLock(); // потокобезопасность
+    private static int accNumber = 0;
+    private static volatile double balance;
+    private static final Lock locker = new ReentrantLock(); // потокобезопасность
 
     BankAccount(int accNumber, double balance) {
-        this.accNumber = accNumber;
-        this.balance = balance;
+        BankAccount.accNumber = accNumber;
+        BankAccount.balance = balance;
     }
 
-    synchronized public void deposit(double amount) { // пополнение счета
+    synchronized static public void depositMoney(Scanner sc, double amount) { // пополнение счета
         locker.lock();
         try {
             if (amount <= 0) throw new IllegalArgumentException("Сумма не может быть отрицательной или равной нулю.");
@@ -25,7 +32,7 @@ public class BankAccount {
         }
     }
 
-    synchronized public void withdraw(double amount) { // вывод средств со счета
+    synchronized static public void withdrawMoney(Scanner sc, double amount) { // вывод средств со счета
         locker.lock();
 
         try {
@@ -39,20 +46,53 @@ public class BankAccount {
         }
     }
 
-    synchronized public void transfer(BankAccount target, double amount) { // перевод средств
-        // необходимо синхронизировать оба класса с клиентами
-        this.locker.lock();
-        target.locker.lock();
+//    synchronized public void transfer(BankAccount target, double amount) { // перевод средств
+//        // необходимо синхронизировать оба класса с клиентами
+//        this.locker.lock();
+//        target.locker.lock();
+//
+//        try {
+//            if (amount <= 0) throw new IllegalArgumentException("\nСумма не может быть отрицательной или равной нулю.\n");
+//            if (amount > balance) throw new IllegalArgumentException("\nНа счете недостаточно средств для снятия.\n");
+//            this.withdraw(amount);
+//            target.deposit(amount);
+//            System.out.printf("\nПереведено %.2f рублей со счёта %d на счёт %d.\n", amount, this.accNumber, target.accNumber);
+//        } finally {
+//            locker.unlock();
+//        }
+//    }
 
-        try {
-            if (amount <= 0) throw new IllegalArgumentException("\nСумма не может быть отрицательной или равной нулю.\n");
-            if (amount > balance) throw new IllegalArgumentException("\nНа счете недостаточно средств для снятия.\n");
-            this.withdraw(amount);
-            target.deposit(amount);
-            System.out.printf("\nПереведено %.2f рублей со счёта %d на счёт %d.\n", amount, this.accNumber, target.accNumber);
-        } finally {
-            locker.unlock();
+    static void transferMoney(Scanner sc, int userId) {
+        System.out.print("Введите ID получателя: ");
+        int receiverId = sc.nextInt();
+        System.out.print("Сумма: ");
+        double amount = sc.nextDouble();
+
+        CompletableFuture.runAsync(() -> {
+            try (Connection conn = Database.getConnection()) {
+                BankAccount from = loadAccount(userId);
+                BankAccount to = loadAccount(receiverId);
+                ClientRequest request = new ClientRequest("Перевести", from, to, amount);
+                new BankSimulator().processClientRequestAsync(request).join();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private static BankAccount loadAccount(int userId) throws IOException {
+        try (Connection conn = Database.getConnection()) {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM accounts WHERE user_id = ?");
+            preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                return new BankAccount(rs.getInt("id"), rs.getDouble("balance"));
+            }
+            throw new SQLException("Аккаунт не найден.");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return BankAccount.loadAccount(0);
     }
 
     synchronized public double checkBalance() {
